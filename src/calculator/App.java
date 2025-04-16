9a4355b65c76279d4ae9fbe66be7b587cc50bca3
Ddiy0ng <bueong0627@gmail.com>
2025-04-16 16:43:37 +0900

    change class name

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -3,7 +3,7 @@ package calculator;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
